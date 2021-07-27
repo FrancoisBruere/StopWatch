e60d46018da397c5d163ba3a017c53a37695013e
Francois Bruere <francois.bruere@gmail.com>
2021-07-27 12:02:43 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -16,19 +16,19 @@ namespace StopWatch
                 var input = Console.ReadLine();
                 Console.Clear();
 
-                if (input == "S")
+                if (input.ToLower() == "S")
                 {
                     Console.WriteLine("Started");
                     stopwatch.Start();
                 }
-                if (input == "T")
+                if (input.ToLower() == "T")
                 {
                     Console.WriteLine("Stopped");
                     stopwatch.Stop();
                     Console.WriteLine("Time lapsed from start to stop: {0}\nTotal Timespan duration: {1}",stopwatch._lapsedTime, stopwatch._duration);
                     
                 }
-                if (input == "Q")
+                if (input.ToLower() == "Q")
                 {
                     Console.WriteLine("Duration: {0}", stopwatch._duration);
                     break;
