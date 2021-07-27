d1b9b82a37d218b08a862280a456f824b6db7a1c
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-07-27 10:43:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var stopwatch = new StopWatch();
+
+            while (true)
+            {
+                Console.WriteLine("Enter (S) to start timer (T) to stop timer (Q) to quit: ");
+                var input = Console.ReadLine();
+                Console.Clear();
+
+                if (input == "S")
+                {
+                    Console.WriteLine("Started");
+                    stopwatch.Start();
+                }
+                if (input == "T")
+                {
+                    Console.WriteLine("Stopped");
+                    stopwatch.Stop();
+                    Console.WriteLine("Time lapsed from start to stop: {0}\nTotal Timespan duration: {1}",stopwatch._lapsedTime, stopwatch._duration);
+                    
+                }
+                if (input == "Q")
+                {
+                    Console.WriteLine("Duration: {0}", stopwatch._duration);
+                    break;
+                }
+
+            }
+
+           
+        }
+    }
+}
+
---
+++ StopWatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+
+        private DateTime _stopTime;
+
+        public TimeSpan _duration { get; private set; }
+        public TimeSpan _lapsedTime { get; private set; }
+
+        private bool _isStarted;
+
+        
+        public void Start()
+        {
+            if (!_isStarted)
+            {
+                _startTime = DateTime.Now;
+                _isStarted = true;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stopwatch already started!! Press any key to exit");
+            }
+        }
+
+        public void Stop()
+        {
+            if (_isStarted)
+            {
+                _stopTime = DateTime.Now;
+                _lapsedTime = _stopTime - _startTime;
+                _duration += _stopTime - _startTime;
+                _isStarted = false;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stopwatch already stopped!! Press any key to exit");
+            }
+
+        }
+
+    }
+}
+
